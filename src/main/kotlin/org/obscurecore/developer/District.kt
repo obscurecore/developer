3e28f10efa734a7cf6b04b9cf2e15c361c5cda7b
ruslanpotapov <gg@gmail>
2025-02-04 16:50:51 +0300

    update schema

--- src/main/kotlin/org/obscurecore/developer/District.kt
+++ src/main/kotlin/org/obscurecore/developer/District.kt
@@ -2,7 +2,12 @@ package org.obscurecore.developer
 
 import io.swagger.v3.oas.annotations.media.Schema
 
-@Schema(description = "Допустимые районы для фильтрации образовательных учреждений")
+@Schema(
+    description = "Допустимые районы для фильтрации образовательных учреждений. " +
+            "Доступные значения: " +
+            "AVIA (Авиастроительный), VAHI (Вахитовский), KIRO (Кировский), " +
+            "MOSC (Московский), NOVO (Ново-Савиновский), PRIV (Приволжский), SOVI (Советский)"
+)
 enum class District(@Schema(description = "Название района", example = "Авиастроительный") val value: String) {
     AVIA("Авиастроительный"),
     VAHI("Вахитовский"),
